a1c6660ee0c9259af01f265bb97274a5b108229a
richardl2003 <richardliy03@gmail.com>
2022-06-15 21:02:52 -0400

    Added a page to display all the playlists

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { accessToken, logout, getCurrentUserProfile } from './spotify'
 import { catchErrors } from './utils'
 import styled from 'styled-components/macro'
 import { GlobalStyle } from './styles';
-import { Login, Profile, TopArtists, TopTracks } from'./pages'
+import { Login, Profile, TopArtists, TopTracks, Playlists } from'./pages'
 
 
 const StyledLogoutButton = styled.button`
@@ -81,7 +81,7 @@ function App() {
                 <h1>Playlist</h1>
               </Route>
               <Route path="/playlists">
-                <h1>Playlists</h1>
+                <Playlists />
               </Route>
               <Route path="/">
                 <Profile />
